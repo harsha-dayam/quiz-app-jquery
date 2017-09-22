1ea3b79f897e1ff27ea54aa1d2c3a57c79890c29
Harsha Dayam <harshavardhan2506@gmail.com>
2017-09-22 14:02:50 -0500

    Edited Server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static(__dirname));
+app.use('/index.html', express.static(__dirname));
 app.listen(3000, function(){ console.log('listening');});
